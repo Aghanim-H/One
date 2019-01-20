1e704a1ec822d2b882cf60fc3fdde6fb12730773
Aghanim-H <1493942143@qq.com>
2019-01-20 21:49:32 +0800

    2019/1/20日修改

--- Python全栈_基础05_集合列表字典元组（北京图灵学院）-操作练习.py
+++ Python全栈_基础05_集合列表字典元组（北京图灵学院）-操作练习.py
@@ -228,3 +228,31 @@ print(list(s))  # list：转换为list
 
 print(list(range(12,19)))
 
+# 传值和传地址的区别
+# 对于简单的数值，采用传值操作，即在函数内对参数的操作不影响外面的变量
+# 对于复杂变量，采用传地址操作，此时函数内的参数和外部变量是同一份内容，任何地方对此内容的更改都影响另外的变量或参数的使用
+def a(n):
+    n[2] = 300
+    print(n)
+    return None
+
+def b(n):
+    n +=100
+    print(n)
+    return None
+
+an = [1,2,3,4,5,6]
+bn = 9
+print(an)
+a(an)
+print(an)
+
+print(bn)
+b(bn)
+print(bn)
+
+
+
+
+
+
