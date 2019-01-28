772612b6659cbc3d250cc1a1b97fe9e7e4d8c6cb
Aghanim-H <1493942143@qq.com>
2019-01-28 22:21:13 +0800

    2019/1/28 修改

--- Python全栈_OOP基础（北京图灵学院）-操作练习.py
+++ Python全栈_OOP基础（北京图灵学院）-操作练习.py
@@ -1,6 +1,7 @@
 '''
 定义一个学生类，用来形容学生
 '''
+# 代码片段1
 # 定义一个空的类
 class Student():
     # 一个空类，pass代表直接跳过，此处pass必须有
@@ -26,6 +27,7 @@ print(yueyue.age)
 # 注意成员函数的调用没有传递进入参数
 yueyue.doHomework()
 
+# 代码片段2
 class Student():
     name = "dana"
     age = 18
@@ -35,6 +37,7 @@ yueyue = Student()
 yueyue.__dict__
 print(yueyue.name)
 
+# 代码片段3
 class A():
     name = "dana"
     age = 18
@@ -73,6 +76,104 @@ print(a.name)
 print(a.age)
 print(id(a.name))
 print(id(a.age))
+
+# 代码片段4
+class Student():
+    name = "dana"
+    age = 18
+    def say(self):
+        self.name = "aaaa"
+        self.age = 200
+        print("My name is {0}".format(self.name))
+        print("My age is {0}".format(self.age))
+yueyue = Student()
+yueyue.say()
+
+class Student():
+    name = "dana"
+    age = 18
+    def sayAgain(s):
+        s.name = "bbbb"
+        s.age = 20
+        print("My name is {0}".format(s.name))
+        print("My age is {0}".format(s.age))
+yueyue = Student()
+yueyue.sayAgain()
+
+class Teacher():
+    name = "dana"
+    age = 19
+    def say(self):
+        self.name = "yaona"
+        self.age = 17
+        print("My name is {0}".format(self.name))
+        print("My age is {0}".format(self.age))
+        # 调用类的成员变量需要用 __class__
+        print("My Age Is {0}".format(__class__.age))
+    def sayAgain():
+        print(__class__.name)
+        print(__class__.age)
+        print("Hello,nice to see you again")
+t = Teacher()
+t.say()
+# 调用绑定类函数使用类名
+# t.sayAgain()  # 此代码会报错
+Teacher.sayAgain()
+
+# 关于self的案例
+class A():
+    name = "liuying"
+    age = 18
+    def __init__(self):
+        self.name = "aaaa"
+        self.age = 200
+    def say(self):
+        print(self.name)
+        print(self.age)
+class B():
+    name = "bbbb"
+    age = 90
+a = A()
+a.say()  # 此时，系统会默认把a作为第一个参数传入函数
+A.say(a)  # 此时self被a替换
+# A.say()  # 会报错
+A.say(A)  # 同样可以把A作为参数传入
+A.say(B)  # 此时，传入的是类实例B，因为B具有name和age属性，所以不会报错
+# 以上代码，利用了鸭子模型
+
+# 代码片段5
+# 私有变量案例
+class Person():
+    # name是共有的成员
+    name = "liuying"
+    # __age就是私有成员
+    __age = 18
+p = Person()
+print(p.name)  # name是公有变量
+# print(p.__age)  # __age是私有变量，注意报错信息
+# name mangling技术
+print(Person.__dict__)
+p._Person__age = 19
+print(p._Person__age)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
