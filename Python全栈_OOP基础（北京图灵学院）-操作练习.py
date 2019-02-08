2a21aed0868e4d7c12ba5add161f0a7d68ba47f1
Aghanim-H <1493942143@qq.com>
2019-02-09 01:47:08 +0800

    2019/2/9 修改

--- Python全栈_OOP基础（北京图灵学院）-操作练习.py
+++ Python全栈_OOP基础（北京图灵学院）-操作练习.py
@@ -638,13 +638,77 @@ Person.say()
 yueyue.say()  # 用实例调用静态方法
 # 作业：自行查找三种方法内存使用方面的区别
 
+# 变量的三种用法
+class A():
+    def __init__(self):
+        self.name = "hahaha"
+        self.age = 18
+a = A()
+# 属性的三种用法
+# 1、赋值
+# 2、读取
+# 3、删除
+a.name = "刘大拿"  # 赋值
+print(a.name)  # 读取
+del a.name  # 删除
+# print(a.name)  # 会报错
+# 类属性 property
+# 应用场景：
+# 对变量除了普遍的三种操作，还想增加一些附加的操作，那么可以通过property完成
+class A():
+    def __init__(self):
+        self.name = "hahahasssss"
+        self.age = 18
+    # 此功能是对类变量进行读取操作的时候应该执行的函数功能
+    def fget(self):
+        print("我被读取了")
+        return self.name
+    # 模拟的是对变量进行写操作的时候执行的功能
+    def fset(self,name):
+        print("我被写入了，但是还可以做好多事情")
+        self.name = "图灵学院：" + name
+    # 模拟的是删除变量的时候进行的操作
+    def fdel(self):
+        pass
+    # property的四个参数顺序是固定的，第一个参数代表读取的时候需要调用的函数，第二个参数代表写入的时候需要调用的函数，第三个是删除
+    name2 = property(fget,fset,fdel,"这是一个property的例子")
+a = A()
+print(a.name)
+print(a.name2)
 
-
-
-
-
-
-
+# 代码片段15
+# 抽象
+class Animal():
+    def sayHello(self):
+        pass
+class Dog(Animal):
+    def sayHello(self):
+        print("闻一下对方")
+class Person(Animal):
+    def sayHello(self):
+        print("Kiss me")
+d = Dog()
+d.sayHello()
+p = Person()
+p.sayHello()
+# 抽象类的实现
+import abc
+# 声明一个类并且指定当前类的元素
+class Huaman(metaclass = abc.ABCMeta):
+    # 定义一个抽象的方法
+    @abc.abstractmethod
+    def smoking(self):
+        pass
+    # 定义类抽象方法
+    @abc.abstractclassmethod
+    def drink():
+        pass
+    # 定义静态抽象方法
+    @abc.abstractstaticmethod
+    def play():
+        pass
+    def sleep(self):
+        print("Sleeping…………")
 
 
 
