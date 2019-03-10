d506c8da1a293af4a1efd79f8a835a8e537158e8
Aghanim-H <1493942143@qq.com>
2019-03-10 22:28:38 +0800

    2019/3/10 增加内容

--- Python全栈（北京图灵学院）-OOP高级04-函数式编程-操作练习.py
+++ Python全栈（北京图灵学院）-OOP高级04-函数式编程-操作练习.py
@@ -40,21 +40,48 @@ def funB(n):
     return funA(n) * 3
 print(funB(9))
 
+# 写一个高阶函数
+def funC(n,f):
+    # 假定函数是把n扩大100倍
+    return f(n) * 3
+print(funC(9,funA))
+# 比较funC和funB，显然funC的写法要优于funB
+# 例如：
+def funD(n):
+    return n * 10
+# 需求变更，需要把n放大三十倍，此时funB则无法实现
+print(funC(7,funD))
 
+# 代码片段5
+# map举例
+# 有一个列表，想对列表里的每一个元素乘以10，并得到新的列表
+l1 = [i for i in range(10)]
+print(l1)
+l2 = []
+for i in l1:
+    l2.append(i * 10)
+print(l2)
 
+# 利用map实现
+def mulTen(n):
+    return n * 10
+l3 = map(mulTen,l1)
+# map类型是一个可迭代的结构，所有可以使用for循环遍历
+for i in l3:
+    print(i)
+# 以下列表生成式得到的结果为空，为什么？
+l4 = [i for i in l3]
+print(l4)
 
-
-
-
-
-
-
-
-
-
-
-
-
+# 代码片段6
+from functools import reduce
+# 定义一个操作函数
+# 加入操作函数只是相加
+def myAdd(x,y):
+    return x + y
+# 对于列表[1,2,3,4,5,6]执行myAdd的reduce操作
+rst = reduce(myAdd,[1,2,3,4,5,6])
+print(rst)
 
 
 
---
+++ 工资测算.py
@@ -0,0 +1,14 @@
+import numpy as np
+# tips = input("请输入你想挣的工资：")
+# # TotalSalary = int(tips)
+# # X表示工资累加总额
+# Y = 100
+# def TotalSalary(X):
+#     for i in range(1,25):
+#         X = 2500*i + Y*20*0.05*i*500*0.3 + 100000*Y*20*0.05*i*0.003*0.05*0.9
+#         print(type(X))
+#         print("第{0}月工资：".format(i),int(X))
+# TotalSalary(int(tips))
+# print("那么你每天需打电话{0}个".format(Y))
+#
+# help(sum)
