2c5fbbadef0cdcca1fbd91ae412b33e859f31a0e
Aghanim-H <1493942143@qq.com>
2019-04-02 23:01:53 +0800

    2019/4/2 增加内容

--- Python全栈（北京图灵学院）-高级语法07-Log/01.py
+++ Python全栈（北京图灵学院）-高级语法07-Log/01.py
@@ -1,4 +1,6 @@
 import logging
+LOG_FORMAT = "%(asctime)s====%(levelname)s+++++%(message)s"
+logging.basicConfig(filename="tulingxueyuan.log",level=logging.DEBUG,format=LOG_FORMAT)
 logging.debug("This is a debug log.")
 logging.info("This is a info log.")
 logging.warning("This is a warning log.")
