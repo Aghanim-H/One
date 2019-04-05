255a997c69db8a663be991eb629eeac8862cc514
Aghanim-H <1493942143@qq.com>
2019-04-05 21:08:20 +0800

    2019/4/5 增加内容3

--- Python全栈（北京图灵学院）-高级语法07-Log/Python全栈（北京图灵学院）-高级语法07-Log-操作练习.py
+++ Python全栈（北京图灵学院）-高级语法07-Log/Python全栈（北京图灵学院）-高级语法07-Log-操作练习.py
@@ -23,12 +23,12 @@ logger = logging.getLogger('mylogger')
 logger.setLevel(logging.DEBUG)
 
 # 为两个不同的文件深圳不同的handler
-rf_handler = logging.handlers.TimedRotatingFileHandler('all.log',when='midninght',interval=1,backupCount=7,atTime=datetime.time(0,0,0,0))
+rf_handler = logging.handlers.TimedRotatingFileHandler('all.log',when='midnight',interval=1,backupCount=7,atTime=datetime.time(0,0,0,0))
 rf_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
 
 f_handler = logging.FileHandler('error.log')
 f_handler.setLevel(logging.ERROR)
-f_handler.setFomatter(logging.Formatter("%(asctime)s - %(levelname)s - $(levelname)s - %(filename)s[:%(lineno)d] - %(message)s"))
+f_handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - $(levelname)s - %(filename)s[:%(lineno)d] - %(message)s"))
 
 # 把相应的处理器组装到logger上
 logger.addHandler(rf_handler)
@@ -38,4 +38,4 @@ logger.debug('debug message')
 logger.info('info message')
 logger.warning('warning message')
 logger.error('error message')
-logger.cirtical('critical message')
+logger.critical('critical message')
